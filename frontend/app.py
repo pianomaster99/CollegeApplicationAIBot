d2fb52afd55f2259097f82c6a40a424744581e91
Tom Liu <liumaizi@gmail.com>
2025-10-26 17:50:36 -0400

    created layout for index.html

--- frontend/app.py
+++ frontend/app.py
@@ -7,6 +7,30 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/essay")
+def essay():
+    return render_template("essay.html")
+
+@app.route("/activities")
+def activities():
+    return render_template("activities.html")
+
+@app.route("/honors")
+def honors():
+    return render_template("honors.html")
+
+@app.route("/testing")
+def testing():
+    return render_template("testing.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
