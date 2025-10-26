19d2149862394354ae0f8e19dd8f2b1e47ad3ecf
Tom Liu <liumaizi@gmail.com>
2025-10-26 17:29:04 -0400

    connected gunicorn to port

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
